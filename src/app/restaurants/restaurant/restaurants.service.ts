9970795f55bb6e4576fb1afdf1cc5556e0f123b1
Julio Cesar <j.juliocesargoncalves@gmail.com>
2021-04-20 17:16:13 -0300

    Fatiamento de template, rotas e serviços

---
+++ src/app/restaurants/restaurant/restaurants.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core"
+import { Restaurant } from "./restaurant.model";
+import { MEAT_API } from "../../app.api";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class RestaurantsService{
+
+
+
+  constructor(private http: Http){}
+
+
+  restaurants() : Observable<Restaurant[]>{
+    return this.http.get(MEAT_API + "/restaurants").map(response => response.json());
+  }
+}
