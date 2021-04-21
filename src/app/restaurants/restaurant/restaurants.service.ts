9ef9482e9028df0d3f7f6985b6117d311f173c3d
Julio Cesar <j.juliocesargoncalves@gmail.com>
2021-04-21 10:41:57 -0300

    Arquivo de rota, api externa

--- src/app/restaurants/restaurant/restaurants.service.ts
+++ src/app/restaurants/restaurant/restaurants.service.ts
@@ -4,16 +4,20 @@ import { MEAT_API } from "../../app.api";
 import { Http } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch"
+import { errorHandler } from "@angular/platform-browser/src/browser";
+import { ErrorHandler } from "app/app.error-handler";
+
 
 @Injectable()
 export class RestaurantsService{
 
-
-
-  constructor(private http: Http){}
+constructor(private http: Http){}
 
 
   restaurants() : Observable<Restaurant[]>{
-    return this.http.get(MEAT_API + "/restaurants").map(response => response.json());
+    return this.http.get(MEAT_API + "/restaurants")
+                    .map(response => response
+                    .json()).catch(ErrorHandler.handleError);
   }
 }
